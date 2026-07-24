8cfc1a7cd3228fbae50baec6704c0c461e87d430
Shivam... <shivam243028@gmail.com>
2026-07-24 23:10:06 +0530

    AgeCalculator

---
+++ Function/AgeCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AgeCalculator {
+
+    static int calculateAge(int birthYear, int currentYear) {
+        return currentYear - birthYear;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Birth Year: ");
+        int birthYear = sc.nextInt();
+
+        System.out.print("Enter Current Year: ");
+        int currentYear = sc.nextInt();
+
+        int age = calculateAge(birthYear, currentYear);
+
+        System.out.println("Your Age is: " + age + " years");
+
+        sc.close();
+    }
+}
