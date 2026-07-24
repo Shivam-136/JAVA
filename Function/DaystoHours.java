40be69bec68057e5f1b8a71f7972f3963b8cab7d
Shivam... <shivam243028@gmail.com>
2026-07-24 23:03:47 +0530

    Days to Hours

---
+++ Function/DaystoHours.java
@@ -0,0 +1,9 @@
+public class DaystoHours{
+
+    static int toHours(int days){
+        return  days * 24;
+    }
+    public static void main(String[] args) {
+        System.out.println(toHours(5));
+    }
+}
