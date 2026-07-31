ee13f1d7c77f79e37c8885d0a6c81f4d9a27e040
Shivam... <shivam243028@gmail.com>
2026-07-31 23:59:37 +0530

    ReverseNumber

--- Function/ReverseNumber.java
+++ Function/ReverseNumber.java
@@ -3,12 +3,10 @@ public class ReverseNumber {
     static int reverse(int num){
 
         int  rev = 0;
-
         while (num !=0){
             rev = rev * 10 + num % 10;
             num /= 10;
         }
-
         return rev;
     }
     public static void main(String[] args) {
