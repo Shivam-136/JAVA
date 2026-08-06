fb76952161ea3fc655cacf7d94c1360906dde434
Shivam... <shivam243028@gmail.com>
2026-08-06 23:50:43 +0530

    PrimeNumber

--- Function/PrimeNumber.java
+++ Function/PrimeNumber.java
@@ -11,7 +11,6 @@ public class PrimeNumber {
         }
         return true;
     }
-
     public static void main(String[] args) {
 
         if (isPrime(13))
--- Function/RemoveSpaces.java
+++ Function/RemoveSpaces.java
@@ -1,11 +1,9 @@
 public class RemoveSpaces{
 
     static String removeSpaces(String str) {
-
         return str.replace(" ", "");
     }
     public static void main(String[] args) {
-
         System.out.println(removeSpaces("Java Programming"));
     }
 }
